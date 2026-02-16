8f76d607d41278b1b39d2e3e2dd0bfa476ef70e7
Omar Dardery <oadardery@gmail.com>
2026-02-16 22:08:40 +0200

    Add new origin to CORS configuration

--- backend/main.go
+++ backend/main.go
@@ -52,7 +52,7 @@ func main() {
 
 	// Configure CORS - must be before routes
 	server.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:3000", "https://solvex.dardery.work"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:3000", "https://solvex.dardery.work", "https://www.solvex-eui.org"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
